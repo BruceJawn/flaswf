e136c7a9a66fb96482c78699b2af08583a319ab9
zhoubu1988@gmail.com <zhoubu1988@gmail.com@e676796f-6653-0410-a89b-2389fba315d8>
2011-09-18 07:13:34 +0000

    GPL

--- Games/GirlSwordFlash/lib/flashSDL.cpp
+++ Games/GirlSwordFlash/lib/flashSDL.cpp
@@ -1,3 +1,25 @@
+	/*
+	* Girl Sword Flash
+	*
+  * Copyright (C) 2011
+  * 吴吉庆 [http://hi.baidu.com/jiqing0925/]
+  * Bruce Jawn [bruce-lab.blogspot.com]
+  *
+	*
+	* Girl Sword Flash is free software; you can redistribute it and/or modify
+	* it under the terms of the GNU Lesser General Public License as published
+	* by the Free Software Foundation; either version 3 of the License, or
+	* (at your option) any later version.
+	*
+	* Girl Sword Flash is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTIMACULAR PURPOSE.  See the
+	* GNU Lesser General Public License for more details.
+	*
+	* You should have received a copy of the GNU Lesser General Public License
+	* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*
+	*/
 #include <stdio.h>
 
 #include "SDL_ttf.h"
