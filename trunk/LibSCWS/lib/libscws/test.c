ae3ddc16161641630cb37da2c4f2e9a89a3ad8de
zhoubu1988@gmail.com <zhoubu1988@gmail.com@e676796f-6653-0410-a89b-2389fba315d8>
2013-10-26 13:14:16 +0000

    LibSCWS

---
+++ trunk/LibSCWS/lib/libscws/test.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+/*char s1[10] = "abcd";
+char s2[10] = "ABCDEF";
+printf("s1 = %s\ns2 = %s\n",s1,s2);
+strncpy(s1,s2,3);
+printf("s1 = %s\ns2 = %s\n",s1,s2);
+*/
+char result[300]={0};
+char text[1000]="哈苏打撒旦as\0";
+char temp[100];
+strncpy(temp, text, 4);
+strcat(result,temp);
+//strcat(result, ' ');
+strncpy(temp, text+6, 4);
+strcat(result,temp);
+printf(" %s",result);
+printf(" %s",text);
+
+char *temp1="哈苏旦";
+char *temp2="打撒";
+char *str;
+
+str = (char*) malloc(strlen(temp1)+1);
+snprintf(str, strlen(temp1), temp1);
+str = (char*) realloc(str, strlen(str)+strlen(temp2)+1);
+snprintf(str+strlen(temp2), strlen(temp2), temp2);
+printf("%s", str);
+
+return 0;
+}
---
+++ trunk/LibSCWS/lib/libscws/trash/Copy of as3api.c
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include "scws.h"
+#include <AS3/AS3.h>
+//http://www.xunsearch.com/scws/docs.php
+// Mark the functions declaration with a GCC attribute specifying the
+// AS3 signature we want it to have in the generated SWC. The functions will
+// be located in the FlasCCTest.lookupeffect namespace.
+
+scws_t s;
+scws_res_t res; 
+scws_res_t cur;
+
+void initialize_SCWS_AS3() __attribute__((used,
+	annotate("as3sig:public function initialize_SCWS_AS3():void"),
+	annotate("as3package:CrossBridge.libscws")));
+
+void initialize_SCWS_AS3()
+{
+	if (!(s = scws_new())) {
+		printf("ERROR: cann't init the scws!\n");
+		//exit(-1);
+	}
+	scws_set_charset(s, "utf8");
+	scws_set_dict(s, "dict.utf8.xdb", SCWS_XDICT_XDB);
+	scws_set_rule(s, "rules.utf8.ini");
+	//scws_send_text(s, text, strlen(text));
+}
+
+void scws_send_text_AS3() __attribute__((used,
+	annotate("as3sig:public function scws_send_text_AS3(inputString:String):String"),
+	annotate("as3package:CrossBridge.libscws")));
+
+void scws_send_text_AS3()
+{
+	char *text = NULL;
+	AS3_MallocString(text, inputString);
+	scws_send_text(s, text, strlen(text));
+	AS3_DeclareVar(myString, String);
+
+	//char *result;
+	//result[0] = '\0';   // ensures the memory is an empty string
+	char result[5000]={"0"};
+    char temp[1000]={'\0'};
+	printf("%s",result);
+	while (res = cur = scws_get_result(s))
+	{
+		while (cur != NULL)
+		{
+			printf("WORD: %.*s/%s (IDF = %4.2f)\n", cur->len, text+cur->off, cur->attr, cur->idf);
+			//if((result = malloc(strlen(result)+ cur->len +1)) != NULL){
+			//if((result = (char*) realloc(strlen(result)+ (cur->len) +1)) != NULL)
+            strncpy(temp, text+cur->off, cur->len);
+			temp[(cur->len)+1]='\0';
+			strcat(result, temp);
+			strcat(result, ' ');
+			strcat(result, '\0');
+            //strncpy(new_str,str2);
+            //} else {
+            //printf("malloc failed!\n");
+            // exit?
+            //}
+			cur = cur->next;
+		}
+		scws_free_result(res);
+	}
+	strcat(result, '\0');
+	
+	printf("%s",result);
+	AS3_CopyCStringToVar(myString, result, strlen(result));
+	scws_free(s);
+	//scws_free(result);
+	AS3_Trace(myString);
+	
+	AS3_Return("212");
+}
+
+void updateUniverse()
+{
+  char *text = "Hello, 我名字叫李那曲是一个中国人, 我有时买Q币来玩, 我还听说过C#语言";
+
+  if (!(s = scws_new())) {
+    printf("ERROR: cann't init the scws!\n");
+    //exit(-1);
+  }
+  scws_set_charset(s, "utf8");
+  scws_set_dict(s, "dict.utf8.xdb", SCWS_XDICT_XDB);
+  scws_set_rule(s, "rules.utf8.ini");
+
+  scws_send_text(s, text, strlen(text));
+  while (res = cur = scws_get_result(s))
+  {
+    while (cur != NULL)
+    {
+      printf("WORD: %.*s/%s (IDF = %4.2f)\n", cur->len, text+cur->off, cur->attr, cur->idf);
+      cur = cur->next;
+    }
+    scws_free_result(res);
+  }
+  scws_free(s);
+}
